92ec0fa681e144fcf4dacb1d610538eb7eba06d1
kozarezov <kozarezov.dd@gmail.com>
2022-12-21 07:22:13 +0300

    initial and users

---
+++ backend/api/v1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+app_name = 'api'
+
+v1_router = DefaultRouter()
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
