bb2f7cc5f4a0c65c72a6dd77034b405024074059
kozarezov <kozarezov.dd@gmail.com>
2023-01-11 18:26:54 +0300

    fix after rewie finish

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -91,26 +91,6 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class TagListField(serializers.RelatedField):
-    """Сериализатор для получения списка тэгов."""
-
-    def to_representation(self, obj):
-        return {
-            'id': obj.id,
-            'name': obj.name,
-            'color': obj.color,
-            'slug': obj.slug
-        }
-
-    def to_internal_value(self, data):
-        try:
-            return Tag.objects.get(id=data)
-        except ObjectDoesNotExist:
-            raise serializers.ValidationError(
-                'Тэг не существует.'
-            )
-
-
 class IngredientSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Ingredient."""
 
@@ -124,8 +104,10 @@ class IngredientsAmountSerializer(serializers.ModelSerializer):
         queryset=Ingredient.objects.all(),
         source='ingredient'
     )
-    name = serializers.SerializerMethodField(read_only=True)
-    measurement_unit = serializers.SerializerMethodField(read_only=True)
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
 
     class Meta:
         model = NumberIngredient
@@ -140,12 +122,6 @@ class IngredientsAmountSerializer(serializers.ModelSerializer):
     def _get_ingredient(self, ingredient_id):
         return get_object_or_404(Ingredient, id=ingredient_id)
 
-    def get_name(self, amount):
-        return self._get_ingredient(amount.ingredient.id).name
-
-    def get_measurement_unit(self, amount):
-        return self._get_ingredient(amount.ingredient.id).measure
-
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Сериализатор для получения списка рецептов."""
@@ -154,6 +130,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(read_only=True, many=True)
     author = UserSerializer(read_only=True)
     ingredients = IngredientsAmountSerializer(
+        source='numberingredient_set',
         many=True,
         read_only=True,
     )
@@ -188,49 +165,61 @@ class CreateUpdateRecipeSerializer(serializers.ModelSerializer):
     image = Base64ImageField(max_length=None, use_url=True)
     author = UserSerializer(read_only=True)
     ingredients = IngredientsAmountSerializer(many=True)
-    tags = TagListField(queryset=Tag.objects.all(), many=True)
+    tags = TagSerializer(read_only=True, many=True)
 
     class Meta:
         model = Recipe
         fields = ('id', 'tags', 'author', 'ingredients', 'is_favorited',
                   'is_in_shopping_cart', 'name', 'image', 'text',
                   'cooking_time')
+        
+    def create_ingredients(self, ingredients, recipe):
+        for ingredient in ingredients:
+            NumberIngredient.objects.create(
+                recipe=recipe,
+                ingredient_id=ingredient.get('id'),
+                amount=ingredient.get('amount'),
+            )
 
     def create(self, validated_data):
-        tags = validated_data.pop('tags')
         image = validated_data.pop('image')
         ingredients = validated_data.pop('ingredients')
         recipe = Recipe.objects.create(image=image, **validated_data)
-        ingredients_list = []
-        for ingredient in ingredients:
-            ingredient_amount, status = (
-                NumberIngredient.objects.get_or_create(**ingredient)
-            )
-            ingredients_list.append(ingredient_amount)
-        recipe.ingredients.set(ingredients_list)
+        tags = self.initial_data.get('tags')
         recipe.tags.set(tags)
+        self.create_ingredients(ingredients, recipe)
         return recipe
 
     def update(self, instance, validated_data):
-        ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
-        instance.name = validated_data.get('name', instance.name)
-        instance.image = validated_data.get('image', instance.image)
-        instance.text = validated_data.get('text', instance.text)
-        instance.cooking_time = validated_data.get(
-            'cooking_time',
-            instance.cooking_time
-        )
-        instance.save()
-        ingredients_list = []
-        for ingredient in ingredients:
-            ingredient_amount, status = (
-                NumberIngredient.objects.get_or_create(**ingredient)
-            )
-            ingredients_list.append(ingredient_amount)
-        instance.ingredients.set(ingredients_list)
+        instance = super().update(instance, validated_data)
+        instance.tags.clear()
         instance.tags.set(tags)
+        NumberIngredient.objects.filter(recipe=instance).all().delete()
+        self.create_ingredients(validated_data.get('ingredients'), instance)
+        instance.save()
         return instance
+    
+    def validate(self, data):
+        ingredients = self.initial_data.get('ingredients')
+        if not ingredients:
+            raise serializers.ValidationError({
+                'ingredients': 'Нужен хоть один ингридиент для рецепта'})
+        ingredient_list = []
+        for ingredient_item in ingredients:
+            ingredient = get_object_or_404(Ingredient,
+                                           id=ingredient_item['id'])
+            if ingredient in ingredient_list:
+                raise serializers.ValidationError('Ингридиенты должны '
+                                                  'быть уникальными')
+            ingredient_list.append(ingredient)
+            if int(ingredient_item['amount']) < 0:
+                raise serializers.ValidationError({
+                    'ingredients': ('Убедитесь, что значение количества '
+                                    'ингредиента больше 0')
+                })
+        data['ingredients'] = ingredients
+        return data
 
     def to_representation(self, instance):
         return RecipeSerializer(instance, context=self.context).data
