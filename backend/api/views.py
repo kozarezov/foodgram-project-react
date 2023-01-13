ccac866fc90872abe997be01bc1034005885ce42
kozarezov <kozarezov.dd@gmail.com>
2023-01-13 10:49:27 +0300

    Delete font

--- backend/api/views.py
+++ backend/api/views.py
@@ -140,8 +140,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 }
             else:
                 final_list[name]['amount'] += item[2]
-        pdfmetrics.registerFont(
-            TTFont('Slimamif', 'Slimamif.ttf', 'UTF-8'))
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = ('attachment; '
                                            'filename="shopping_list.pdf"')
