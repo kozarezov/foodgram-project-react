9da21cf858f169b237fda8eee8e5f864740af491
kozarezov <kozarezov.dd@gmail.com>
2023-01-13 08:52:57 +0300

    new bd

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Sum
 from django.http.response import HttpResponse
 from djoser.views import UserViewSet
 from reportlab.pdfbase import pdfmetrics
